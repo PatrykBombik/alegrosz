1259d2e62d5699e1a703aed3f378b8d878119cf4
PatrykBombik <patryk.bombik@gmail.com>
2023-05-31 15:26:41 +0200

    add responsive app bar

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { RouterProvider } from "react-router-dom";
-import { router } from "./helpers/router";
+import CssBaseline from "@mui/material/CssBaseline";
+
+import { router } from "./helpers/router.jsx";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
     <React.StrictMode>
+        <CssBaseline />
         <RouterProvider router={router} />
     </React.StrictMode>
 );
