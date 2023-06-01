d171c7d1f1c84f26b5837b918ca79d3dda097c79
PatrykBombik <patryk.bombik@gmail.com>
2023-06-01 16:26:12 +0200

    sort by price using query params from react router dom

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,25 @@
-import { useEffect, useState } from "react";
+import React, { useEffect, useState } from "react";
 import Grid from "@mui/material/Grid";
 import ProductSearch from "./components/ProductSearch.jsx";
 import ProductList from "./components/ProductList.jsx";
 import Container from "@mui/material/Container";
+import FormControl from "@mui/material/FormControl";
+import { InputLabel } from "@mui/material";
+import Select from "@mui/material/Select";
+import MenuItem from "@mui/material/MenuItem";
+import { useSearchParams } from "react-router-dom";
 
 function App() {
+    const [searchParams, setSearchParams] = useSearchParams();
     const [products, setProducts] = useState([]);
     const [search, setSearch] = useState("");
+    const [sortByPrice, setSortByPrice] = useState(
+        searchParams.get("sortBy") || "none"
+    );
+
+    useEffect(() => {
+        setSearchParams({ sortBy: sortByPrice });
+    }, [sortByPrice]);
 
     useEffect(() => {
         getProducts().then((data) => setProducts(data));
@@ -24,15 +37,39 @@ function App() {
                     <Grid item xs={12}>
                         <h1>Alegrosz</h1>
                     </Grid>
-                    <Grid item xs={12}>
+                    <Grid item xs={4}>
                         <ProductSearch search={search} setSearch={setSearch} />
+                    </Grid>
+                    <Grid item xs={4}>
+                        <FormControl fullWidth>
+                            <InputLabel id="sortByPrice">
+                                Sort by price
+                            </InputLabel>
+                            <Select
+                                labelId="sortByPrice"
+                                id="sortByPrice"
+                                value={sortByPrice}
+                                label="Sort by price"
+                                onChange={(event) =>
+                                    setSortByPrice(event.target.value)
+                                }
+                            >
+                                <MenuItem value={"none"}>---</MenuItem>
+                                <MenuItem value={"asc"}>Ascending</MenuItem>
+                                <MenuItem value={"dsc"}>Descending</MenuItem>
+                            </Select>
+                        </FormControl>
                     </Grid>
                     <Grid
                         item
                         xs={12}
                         sx={{ display: "flex", flexWrap: "wrap", gap: "10px" }}
                     >
-                        <ProductList products={products} search={search} />
+                        <ProductList
+                            products={products}
+                            search={search}
+                            sortByPrice={sortByPrice}
+                        />
                     </Grid>
                 </Grid>
             </Container>
--- src/components/ProductList.jsx
+++ src/components/ProductList.jsx
@@ -1,7 +1,7 @@
 import ProductCard from "./ProductCard.jsx";
 import PropTypes from "prop-types";
 
-function ProductList({ products, search }) {
+function ProductList({ products, search, sortByPrice }) {
     return (
         <>
             {products
@@ -10,6 +10,22 @@ function ProductList({ products, search }) {
                         .toLowerCase()
                         .includes(search.toLowerCase())
                 )
+                .sort((a, b) => {
+                    // if (sortByPrice === 1) {
+                    //     return a.price - b.price;
+                    // } else if (sortByPrice === 2) {
+                    //     return b.price - a.price;
+                    // }
+                    // return 0;
+                    switch (sortByPrice) {
+                        case "asc":
+                            return a.price - b.price;
+                        case "dsc":
+                            return b.price - a.price;
+                        default:
+                            return 0;
+                    }
+                })
                 .map((product) => (
                     <ProductCard key={product.id} product={product} />
                 ))}
@@ -20,6 +36,7 @@ function ProductList({ products, search }) {
 ProductList.propTypes = {
     products: PropTypes.array.isRequired,
     search: PropTypes.string.isRequired,
+    sortByPrice: PropTypes.number.isRequired,
 };
 
 export default ProductList;
